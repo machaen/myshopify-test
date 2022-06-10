4cba4844334f275a4978244aa2cda641e0357074
= <=>
2022-06-10 17:16:59 -0500

    Updates

---
+++ precache-manifest.29ad20ad57ab9a03664a5c5e617776d5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68807cec1a0fb6a7818a24d34442b925",
+    "url": "/myshopify-test/index.html"
+  },
+  {
+    "revision": "17a3c7da94e10e047f6a",
+    "url": "/myshopify-test/static/css/2.981ae53e.chunk.css"
+  },
+  {
+    "revision": "bed85c5ca67312b8fac3",
+    "url": "/myshopify-test/static/css/main.c76c6908.chunk.css"
+  },
+  {
+    "revision": "17a3c7da94e10e047f6a",
+    "url": "/myshopify-test/static/js/2.078bf379.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/myshopify-test/static/js/2.078bf379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed85c5ca67312b8fac3",
+    "url": "/myshopify-test/static/js/main.65f501b2.chunk.js"
+  },
+  {
+    "revision": "14f0f86163c18fb3277a",
+    "url": "/myshopify-test/static/js/runtime-main.f1a1a0a4.js"
+  },
+  {
+    "revision": "5f5a5c14df7549aab218c080d0e49b72",
+    "url": "/myshopify-test/static/media/group-10.5f5a5c14.png"
+  },
+  {
+    "revision": "8dc42d2dd5674cddaf92cb059941ebb9",
+    "url": "/myshopify-test/static/media/helveticaneue.8dc42d2d.woff2"
+  },
+  {
+    "revision": "0fbe92808d6174c935f018bdf25b0f3a",
+    "url": "/myshopify-test/static/media/logo.0fbe9280.svg"
+  },
+  {
+    "revision": "1655105a1d5b17c21bde9154207da8ab",
+    "url": "/myshopify-test/static/media/main.1655105a.jpg"
+  },
+  {
+    "revision": "01082f95b0bf3f6d27b150404960cd3b",
+    "url": "/myshopify-test/static/media/our-rackets.01082f95.jpg"
+  },
+  {
+    "revision": "96272d7350f7816a001e6a52276a6c9c",
+    "url": "/myshopify-test/static/media/prefooter.96272d73.jpg"
+  },
+  {
+    "revision": "0afb16304086e6821bdabe9801dd11d1",
+    "url": "/myshopify-test/static/media/social-facebook.0afb1630.svg"
+  },
+  {
+    "revision": "c49a4202f724153b12bf950692cd85f0",
+    "url": "/myshopify-test/static/media/social-instagram.c49a4202.svg"
+  },
+  {
+    "revision": "1ff8881de155b45a08ad3334867fbbc5",
+    "url": "/myshopify-test/static/media/social-linkedin.1ff8881d.svg"
+  },
+  {
+    "revision": "216f5101cbfc83623574b720bfbb42ab",
+    "url": "/myshopify-test/static/media/social-twitter.216f5101.svg"
+  },
+  {
+    "revision": "1a68005075c0a4db053ebcf8c9a23b0d",
+    "url": "/myshopify-test/static/media/social-vimeo.1a680050.svg"
+  }
+]);
